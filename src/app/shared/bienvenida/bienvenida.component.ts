222493c10aaec20c150f61500b0e10a42639eb6e
VM17015 <VM17015@ues.edu.sv>
2021-06-11 12:07:49 -0600

    Componente de bienvenida con display del nombre de usuario

---
+++ src/app/shared/bienvenida/bienvenida.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  selector: 'app-bienvenida',
+  templateUrl: './bienvenida.component.html',
+  styleUrls: ['./bienvenida.component.css']
+})
+export class BienvenidaComponent implements OnInit {
+
+  get usuario() {
+    return this.authSvc.usuario;
+  }
+
+  constructor(
+    private authSvc: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
